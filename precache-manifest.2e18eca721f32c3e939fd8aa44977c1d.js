4fae85cc9f79bc68736a9214dbb8bac0d029113e
Danylenko <fedirdanylenko@gmail.com>
2024-06-12 16:46:27 +0300

    Updates

---
+++ precache-manifest.2e18eca721f32c3e939fd8aa44977c1d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa707fbf3f3e4eb6a92c660b0d637324",
+    "url": "/Crediting/index.html"
+  },
+  {
+    "revision": "f1d90f7eacd7f91ce442",
+    "url": "/Crediting/static/css/main.e9dd93e3.chunk.css"
+  },
+  {
+    "revision": "c72feb6dc286339a449b",
+    "url": "/Crediting/static/js/2.38fec76d.chunk.js"
+  },
+  {
+    "revision": "f1d90f7eacd7f91ce442",
+    "url": "/Crediting/static/js/main.a7560e2f.chunk.js"
+  },
+  {
+    "revision": "bc72c3e3ab2d9807d39b",
+    "url": "/Crediting/static/js/runtime~main.dcf9ff0b.js"
+  }
+]);
